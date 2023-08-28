ca4048383143da6366ed6f37a38a1360e1414fcf
Hasan Faraz Zaidi <hasanfarazzaidi@Hasans-MacBook-Air.local>
2023-08-28 15:43:45 +0530

    First commit

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React  from "react";
 import ReactDOM  from "react-dom/client";
-const heading = React.createElement('h1',{},"Hello, React!!!");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+import Header from "./header";
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+    <div>
+        <Header/>
+    </div>
+);
